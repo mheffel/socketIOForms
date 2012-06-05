97246a0f7fc0fe2666f459b010c34d4f8f36d648
mheffel <mheffel@bellsouth.net>
2012-06-05 08:44:29 -0500

    first commit

---
+++ connect.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+app.use(express.bodyParser());
+//io.set('log level', 1);
+
+app.listen(3000);
+
+app.use(express.static(__dirname));
+
+app.post('/', function(req, res){
+  console.log(req.body);
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  res.redirect('back');
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log("In connection");
+//  socket.emit('news', { hello: 'world' });
+//  socket.broadcast.emit('news', message);
+
+  socket.on('newvalue', function (data) {
+    console.log("In newvalue");
+    console.log(data);
+    io.sockets.emit('update', data);
+  });
+});
+
+
+
+
